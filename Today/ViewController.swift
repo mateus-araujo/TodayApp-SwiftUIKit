d59123a74b1f576106d9e7f6e553480b12820777
Mateus Araujo <rb.mateus.araujo@gmail.com>
2024-12-30 15:19:26 -0300

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Mateus Araújo on 30/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodayTests/TodayTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodayTests.swift
+//  TodayTests
+//
+//  Created by Mateus Araújo on 30/12/24.
+//
+
+import Testing
+@testable import Today
+
+struct TodayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
