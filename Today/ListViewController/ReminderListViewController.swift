5c4d0a1a34a98725f0bb23782f6e78f334874e5e
Mateus Araujo <rb.mateus.araujo@gmail.com>
2025-01-04 18:43:41 -0300

    Collection views and navigation: Displaying cell info

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -9,29 +9,15 @@ import UIKit
 
 
 class ReminderListViewController: UICollectionViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
-    
-    
     var dataSource: DataSource!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
         
-        
-        let cellRegistration = UICollectionView.CellRegistration {
-            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
-            let reminder = Reminder.sampleData[indexPath.item]
-            var contentConfiguration = cell.defaultContentConfiguration()
-            contentConfiguration.text = reminder.title
-            cell.contentConfiguration = contentConfiguration
-        }
-        
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
         
         dataSource = DataSource(collectionView: collectionView) {
             (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: String) in
@@ -39,12 +25,10 @@ class ReminderListViewController: UICollectionViewController {
                 using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
         
-        
         var snapshot = Snapshot()
         snapshot.appendSections([0])
         snapshot.appendItems(Reminder.sampleData.map { $0.title })
         dataSource.apply(snapshot)
-        
         
         collectionView.dataSource = dataSource
     }
