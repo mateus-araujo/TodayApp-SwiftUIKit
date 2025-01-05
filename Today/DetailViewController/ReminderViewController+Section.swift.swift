d000d1053dfaf67848a04d3164d3927a7f556184
Mateus Araujo <rb.mateus.araujo@gmail.com>
2025-01-05 18:30:00 -0300

    Making editable views: Getting ready for editing

---
+++ Today/DetailViewController/ReminderViewController+Section.swift.swift
@@ -0,0 +1,29 @@
+//
+//  ReminderViewController+Section.swift.swift
+//  Today
+//
+//  Created by Mateus Araújo on 05/01/25.
+//
+
+import Foundation
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+        
+        var name: String {
+            switch self {
+            case .view: return ""
+            case .title:
+                return NSLocalizedString("Title", comment: "Title section name")
+            case .date:
+                return NSLocalizedString("Date", comment: "Date section name")
+            case .notes:
+                return NSLocalizedString("Notes", comment: "Notes section name")
+            }
+        }
+    }
+}
