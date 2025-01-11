075b7d0456a3e724c84fb6f68f698cfb996cb0cf
Mateus Araujo <rb.mateus.araujo@gmail.com>
2025-01-11 18:14:21 -0300

    Using content views: Create content views for the title, date and notes

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  Configuration+Stateless.swift
+//  Today
+//
+//  Created by Mateus Araújo on 11/01/25.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
